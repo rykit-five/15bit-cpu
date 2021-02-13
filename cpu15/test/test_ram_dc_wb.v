4f8a2956b2a84712f8983239b63a97846227243c
Ryota Kitagawa <doal.r2@gmail.com>
2021-02-13 11:03:41 +0900

    ram_dc_wbテストベンチを実装

--- cpu15/test/test_ram_dc_wb.v
+++ cpu15/test/test_ram_dc_wb.v
@@ -1,4 +1,4 @@
-`include "../ram_dc_wb.v"
+// `include "../ram_dc_wb.v"
 
 module RAM_DC_WB_TEST();
     reg             clk_dc;
@@ -6,7 +6,7 @@ module RAM_DC_WB_TEST();
     reg     [15:0]  ram_addr;
     reg     [15:0]  ram_in;
     reg     [15:0]  io65_in;
-    reg;            ram_wen;
+    reg             ram_wen;
     wire    [15:0]  ram_out;
     wire    [15:0]  io64_out;
 
@@ -22,20 +22,9 @@ module RAM_DC_WB_TEST();
     );
 
     initial begin
-        $dumpfile("test_ram_dc_wb.vcd");
+        $dumpfile("./test/test_ram_dc_wb.vcd");
         $dumpvars(0, ram_dc_wb_inst);
         $monitor("%t: ram_addr=%h, ram_in=%h, io65_in=%h => ram_wen=%b, ram_out=%h, io64_out=%h", $time, ram_addr, ram_in, io65_in, ram_wen, ram_out, io64_out);
-    end
-
-    // テスト信号の発生
-    initial begin
-        clk_dc <= 1'b0;
-        clk_wb <= 1'b0;
-        ram_addr <= 16'h0000;
-        io65_in <= 16'h0000;
-        ram_wen <= 1'b1;
-        ram_out <= 16'h0000;
-        io64_out <= 16'h0000;
     end
 
     always #5 begin
@@ -50,7 +39,6 @@ module RAM_DC_WB_TEST();
         clk_dc <= 1'b1;
       #10
         clk_dc <= 1'b0;
-      #20
     end
 
     // 入力信号clk_wbの波形
@@ -58,7 +46,46 @@ module RAM_DC_WB_TEST();
         clk_wb <= 1'b0;
       #30
         clk_dc <= 1'b1;
-      #10
+      
+    end
+
+    // 入力信号ram_addrの波形
+    integer   i, j;
+    initial begin
+        // integer   i, j;
+        for (i = 0; i < 1; i = i + 1) begin
+            ram_addr <= 16'b0000000000000000;
+            for (j = 0; j < 16; j = j + 1) begin
+              #40
+                ram_addr <= ram_addr + 16'b0000000000000001;
+            end
+        end
+    end
+
+    // 入力信号ram_inの波形
+    initial begin
+        ram_in <= 16'b0000000000000010;
+      #40
+        ram_in <= 16'b0000000000000100;
+      #40
+        ram_in <= 16'b0000000000000110;
+      #40
+        ram_in <= 16'b0000000000001000;
+      #40
+        ram_in <= 16'b0000000000000111;
+      #40
+        ram_in <= 16'b0000000000000101;
+      #40
+        ram_in <= 16'b0000000000000011;
+    end
+
+    // 入力信号ram_wenの波形
+    initial begin
+        ram_wen <= 1'b1;
+      #640
+        ram_wen <= 1'b0;
+      #640
+      $finish;
     end
 
 endmodule
